bce2c9a098b03b6ddfda1cdb577532c0275fc30e
bimlas <bimbalaszlo@gmail.com>
2020-01-28 19:25:18 +0100

    Reformat Javascript code to Tiddly style

--- plugins/locator/macros/javascript-filters.js
+++ plugins/locator/macros/javascript-filters.js
@@ -6,265 +6,265 @@ module-type: filteroperator
 Special filters used by Locator
 
 \*/
-(function () {
+(function() {
 
-    /*jslint node: true, browser: true */
-    /*global $tw: true */
-    "use strict";
+	/*jslint node: true, browser: true */
+	/*global $tw: true */
+	"use strict";
 
-    function getFieldListingOperator(options, field) {
-        var fieldOptionsTiddler = "$:/config/bimlas/locator/fields/" + field;
+	function getFieldListingOperator(options,field) {
+		var fieldOptionsTiddler = "$:/config/bimlas/locator/fields/" + field;
 
-        return options.wiki.getCacheForTiddler(fieldOptionsTiddler, "fieldListingOperator", function() {
-            var fieldOptions = options.wiki.getTiddler(fieldOptionsTiddler);
+		return options.wiki.getCacheForTiddler(fieldOptionsTiddler,"fieldListingOperator",function() {
+			var fieldOptions = options.wiki.getTiddler(fieldOptionsTiddler);
 
-            return fieldOptions && fieldOptions.fields["field-type"] === "list"
-                ? "contains"
-                : "field";
-        });
-    }
+			return fieldOptions && fieldOptions.fields["field-type"] === "list"
+				? "contains"
+				: "field";
+		});
+	}
 
-    function getFieldDirection(options, field) {
-        var fieldOptions = options.wiki.getTiddler("$:/config/bimlas/locator/fields/" + field);
+	function getFieldDirection(options,field) {
+		var fieldOptions = options.wiki.getTiddler("$:/config/bimlas/locator/fields/" + field);
 
-        return (fieldOptions || {fields: {}}).fields["field-direction"];
-    }
+		return (fieldOptions || {fields: {}}).fields["field-direction"];
+	}
 
-    function getActiveFilters(options, filterState) {
-        return options.wiki.getCacheForTiddler(filterState, "activeFilters", function() {
-            var filteredFields = options.wiki.getTiddlerDataCached(filterState, {});
-            var results = {};
+	function getActiveFilters(options,filterState) {
+		return options.wiki.getCacheForTiddler(filterState,"activeFilters",function() {
+			var filteredFields = options.wiki.getTiddlerDataCached(filterState,{});
+			var results = {};
 
-            $tw.utils.each(filteredFields, function(valuesAsString, field) {
-                var values = $tw.utils.parseStringArray(valuesAsString) || [];
-                if(values.length) {
-                    results[field] = values;
-                }
-            });
+			$tw.utils.each(filteredFields,function(valuesAsString,field) {
+				var values = $tw.utils.parseStringArray(valuesAsString) || [];
+				if(values.length) {
+					results[field] = values;
+				}
+			});
 
-            return results;
-        });
-    }
+			return results;
+		});
+	}
 
-    function applyFieldsFilters(source, options, filterState, filterFunc, prefix) {
-        var filterOperators = options.wiki.getFilterOperators();
-        var activeFilters = getActiveFilters(options, filterState);
-        var results = source;
+	function applyFieldsFilters(source,options,filterState,filterFunc,prefix) {
+		var filterOperators = options.wiki.getFilterOperators();
+		var activeFilters = getActiveFilters(options,filterState);
+		var results = source;
 
-        if (!Object.keys(activeFilters).length) return results;
+		if(!Object.keys(activeFilters).length) return results;
 
-        $tw.utils.each(activeFilters, function (values, field) {
-            $tw.utils.each(values, function (value) {
-                if (value === "ANY-VALUE") {
-                    results = filterOperators["has"](results, { operand: field, prefix: prefix }, options);
-                } else {
-                    results = filterFunc(filterOperators, results, field, value, prefix);
-                }
-                results = options.wiki.makeTiddlerIterator(results);
-            });
-        });
+		$tw.utils.each(activeFilters,function(values,field) {
+			$tw.utils.each(values,function(value) {
+				if(value === "ANY-VALUE") {
+					results = filterOperators["has"](results,{operand: field,prefix: prefix},options);
+				} else {
+					results = filterFunc(filterOperators,results,field,value,prefix);
+				}
+				results = options.wiki.makeTiddlerIterator(results);
+			});
+		});
 
-        return results;
-    }
+		return results;
+	}
 
-    function getDirectionOfTraverse(options, contextState, fieldOfRelationship) {
-        var contextStateTiddler = options.wiki.getTiddler(contextState) || {fields: []};
-        var fieldSettings = options.wiki.getTiddler("$:/config/bimlas/locator/fields/" + fieldOfRelationship) || {fields: []};
-        var direction = fieldSettings.fields["field-direction"];
-        if (contextStateTiddler.fields["invert-direction"] === "yes") {
-            direction = ["from", "to"][(direction === "from") + 0];
-        }
-        return direction;
-    }
+	function getDirectionOfTraverse(options,contextState,fieldOfRelationship) {
+		var contextStateTiddler = options.wiki.getTiddler(contextState) || {fields: []};
+		var fieldSettings = options.wiki.getTiddler("$:/config/bimlas/locator/fields/" + fieldOfRelationship) || {fields: []};
+		var direction = fieldSettings.fields["field-direction"];
+		if(contextStateTiddler.fields["invert-direction"] === "yes") {
+			direction = ["from","to"][(direction === "from") + 0];
+		}
+		return direction;
+	}
 
-    /*
-    Filter titles matching to Locator fields filter
+	/*
+	Filter titles matching to Locator fields filter
 
-    Input: list of tiddlers
-    Param: filterState
-    Prefix: "!" to exclude matching tiddlers
-    Suffix: "recusive" enables recursive filtering
-    */
-    exports["locator-fields-filter"] = function (source, operator, options) {
-        var results = source;
-        var activeRecursiveFilters = getActiveFilters(options, "$:/state/bimlas/locator/search/recursive-filters/");
+	Input: list of tiddlers
+	Param: filterState
+	Prefix: "!" to exclude matching tiddlers
+	Suffix: "recusive" enables recursive filtering
+	*/
+	exports["locator-fields-filter"] = function(source,operator,options) {
+		var results = source;
+		var activeRecursiveFilters = getActiveFilters(options,"$:/state/bimlas/locator/search/recursive-filters/");
 
-        if (operator.suffix === "recursive") {
-            results = applyFieldsFilters(results, options, operator.operand, recursiveFilterFunc, operator.prefix);
-        } else {
-            results = applyFieldsFilters(results, options, operator.operand, directFilterFunc, operator.prefix);
-        }
+		if(operator.suffix === "recursive") {
+			results = applyFieldsFilters(results,options,operator.operand,recursiveFilterFunc,operator.prefix);
+		} else {
+			results = applyFieldsFilters(results,options,operator.operand,directFilterFunc,operator.prefix);
+		}
 
-        return results;
+		return results;
 
-        function directFilterFunc(filterOperators, input, field, value, prefix) {
-            var fieldListingOperator = getFieldListingOperator(options, field);
-            return filterOperators[fieldListingOperator](input, { operand: value, prefix: prefix, suffix: field }, options);
-        }
+		function directFilterFunc(filterOperators,input,field,value,prefix) {
+			var fieldListingOperator = getFieldListingOperator(options,field);
+			return filterOperators[fieldListingOperator](input,{operand: value,prefix: prefix,suffix: field},options);
+		}
 
-        function recursiveFilterFunc(filterOperators, input, field, value, prefix) {
-            var isRecursiveFilteringActive = $tw.utils.hop(activeRecursiveFilters, field) && (activeRecursiveFilters[field].indexOf(value) >= 0);
-            if (isRecursiveFilteringActive) {
-                var fieldDirection = getFieldDirection(options, field);
-                return filterOperators.kin(input, { operand: value, prefix: prefix, suffixes: [[field], [fieldDirection]] }, options);
-            } else {
-                return directFilterFunc(filterOperators, input, field, value, prefix)
-            }
-        }
-    };
+		function recursiveFilterFunc(filterOperators,input,field,value,prefix) {
+			var isRecursiveFilteringActive = $tw.utils.hop(activeRecursiveFilters,field) && (activeRecursiveFilters[field].indexOf(value) >= 0);
+			if(isRecursiveFilteringActive) {
+				var fieldDirection = getFieldDirection(options,field);
+				return filterOperators.kin(input,{operand: value,prefix: prefix,suffixes: [[field],[fieldDirection]]},options);
+			} else {
+				return directFilterFunc(filterOperators,input,field,value,prefix);
+			}
+		}
+	};
 
-    /*
-    Filter fields that are not disabled in Locator field options
+	/*
+	Filter fields that are not disabled in Locator field options
 
-    Input: list of fields
-    Param (optional): contextState
-    */
-    exports["locator-enabled-fields"] = function (source, operator, options) {
-        var mainMacro = operator.operand.replace(/^\$:\/state\/bimlas\/locator\//, "").replace(/\/.*/, "") || "search";
-        var excludedFields = options.wiki.filterTiddlers("[all[tiddlers+shadows]field:hide-in-" + mainMacro + "-fields-filter[yes]removeprefix[$:/config/bimlas/locator/fields/]]") || [];
-        var results = [];
+	Input: list of fields
+	Param (optional): contextState
+	*/
+	exports["locator-enabled-fields"] = function(source,operator,options) {
+		var mainMacro = operator.operand.replace(/^\$:\/state\/bimlas\/locator\//,"").replace(/\/.*/,"") || "search";
+		var excludedFields = options.wiki.filterTiddlers("[all[tiddlers+shadows]field:hide-in-" + mainMacro + "-fields-filter[yes]removeprefix[$:/config/bimlas/locator/fields/]]") || [];
+		var results = [];
 
-        source(function (tiddler, title) {
-            if (excludedFields.indexOf(title) < 0) {
-                results.push(title)
-            }
-        });
+		source(function(tiddler,title) {
+			if(excludedFields.indexOf(title) < 0) {
+				results.push(title);
+			}
+		});
 
-        return results;
-    };
+		return results;
+	};
 
-    /*
-    List fields which can be used to build tree ("tags" for example)
+	/*
+	List fields which can be used to build tree ("tags" for example)
 
-    Input: none
-    Param (optional): field to check if it's a relationship field
-    */
-    exports["locator-enlist-relationship-fields"] = function (source, operator, options) {
-        var relationshipFields = options.wiki.getGlobalCache("bimlas-locator-enlist-relationship-fields", function() {
-            return options.wiki.filterTiddlers("[all[tiddlers+shadows]prefix[$:/config/bimlas/locator/fields/]has[field-direction]removeprefix[$:/config/bimlas/locator/fields/]]");
-        });
+	Input: none
+	Param (optional): field to check if it's a relationship field
+	*/
+	exports["locator-enlist-relationship-fields"] = function(source,operator,options) {
+		var relationshipFields = options.wiki.getGlobalCache("bimlas-locator-enlist-relationship-fields",function() {
+			return options.wiki.filterTiddlers("[all[tiddlers+shadows]prefix[$:/config/bimlas/locator/fields/]has[field-direction]removeprefix[$:/config/bimlas/locator/fields/]]");
+		});
 
-        if (operator.operand) {
-            return relationshipFields.indexOf(operator.operand) >= 0
-                ? [operator.operand]
-                : [];
-        }
+		if(operator.operand) {
+			return relationshipFields.indexOf(operator.operand) >= 0
+				? [operator.operand]
+				: [];
+		}
 
-        return relationshipFields;
-    };
+		return relationshipFields;
+	};
 
-    /*
-    List field values according to Locator field settings
+	/*
+	List field values according to Locator field settings
 
-    Input: list of tiddlers
-    Param: field
-    */
-    exports["locator-enlist-field-values"] = function (source, operator, options) {
-        var fieldListingOperator = getFieldListingOperator(options, operator.operand);
-        var results = [];
+	Input: list of tiddlers
+	Param: field
+	*/
+	exports["locator-enlist-field-values"] = function(source,operator,options) {
+		var fieldListingOperator = getFieldListingOperator(options,operator.operand);
+		var results = [];
 
-        source(function (tiddler, title) {
-            if(!tiddler) return;
+		source(function(tiddler,title) {
+			if(!tiddler) return;
 
-            var value = tiddler.fields[operator.operand];
-            if(fieldListingOperator === "contains") {
-                value = $tw.utils.parseStringArray(value);
-            }
+			var value = tiddler.fields[operator.operand];
+			if(fieldListingOperator === "contains") {
+				value = $tw.utils.parseStringArray(value);
+			}
 
-            if(!value) return;
+			if(!value) return;
 
-            results = $tw.utils.pushTop(results,value);
-        });
+			results = $tw.utils.pushTop(results,value);
+		});
 
-        return results;
-    };
+		return results;
+	};
 
-    /*
-    List of active field filters
+	/*
+	List of active field filters
 
-    Input: filterState
-    Param (optional): field
-    */
-    exports["locator-selected-field-values"] = function (source, operator, options) {
-        var activeFilters = {};
+	Input: filterState
+	Param (optional): field
+	*/
+	exports["locator-selected-field-values"] = function(source,operator,options) {
+		var activeFilters = {};
 
-        source(function (tiddler, title) {
-            $tw.utils.each(getActiveFilters(options, title), function(value, key) {
-                activeFilters[key] = $tw.utils.pushTop(activeFilters[key] || [], value);
-            });
-        });
+		source(function(tiddler,title) {
+			$tw.utils.each(getActiveFilters(options,title),function(value,key) {
+				activeFilters[key] = $tw.utils.pushTop(activeFilters[key] || [],value);
+			});
+		});
 
-        if (!Object.keys(activeFilters).length) return [];
+		if(!Object.keys(activeFilters).length) return [];
 
-        return operator.operand
-            ? activeFilters[operator.operand] || []
-            : ["TODO: Join active filter values (array of arrays)"];
-    };
+		return operator.operand
+			? activeFilters[operator.operand] || []
+			: ["TODO: Join active filter values (array of arrays)"];
+	};
 
-    /*
-    List of active field names
+	/*
+	List of active field names
 
-    Input: filterState
-    Param (optional): none
-    */
-    exports["locator-selected-field-names"] = function (source, operator, options) {
-        var fieldNames = [];
+	Input: filterState
+	Param (optional): none
+	*/
+	exports["locator-selected-field-names"] = function(source,operator,options) {
+		var fieldNames = [];
 
-        source(function (tiddler, title) {
-            fieldNames = $tw.utils.pushTop(fieldNames, Object.keys(getActiveFilters(options, title)));
-        });
+		source(function(tiddler,title) {
+			fieldNames = $tw.utils.pushTop(fieldNames,Object.keys(getActiveFilters(options,title)));
+		});
 
-        return fieldNames;
-    };
+		return fieldNames;
+	};
 
-    /*
-    List children of input elements based on selected relationship field
+	/*
+	List children of input elements based on selected relationship field
 
-    Input: parent tiddlers
-    Param: contextState
-    Suffix: field of relationship
-    */
-    exports["locator-enlist-children"] = function (source, operator, options) {
-        var fieldOfRelationship = operator.suffix;
-        var directionOfTraverse = getDirectionOfTraverse(options, operator.operand, fieldOfRelationship);
-        var results = [];
+	Input: parent tiddlers
+	Param: contextState
+	Suffix: field of relationship
+	*/
+	exports["locator-enlist-children"] = function(source,operator,options) {
+		var fieldOfRelationship = operator.suffix;
+		var directionOfTraverse = getDirectionOfTraverse(options,operator.operand,fieldOfRelationship);
+		var results = [];
 
-        source(function (tiddler, title) {
-            results = $tw.utils.pushTop(results, fieldOfRelationship === "LINKS-IN-TEXT"
-                ? byLinksInText(title)
-                : byField(title)
-            );
-        });
+		source(function(tiddler,title) {
+			results = $tw.utils.pushTop(results,fieldOfRelationship === "LINKS-IN-TEXT"
+				? byLinksInText(title)
+				: byField(title)
+			);
+		});
 
-        return results;
+		return results;
 
-        function byLinksInText(title) {
-            return directionOfTraverse === "to"
-                ? options.wiki.getTiddlerBacklinks(title)
-                : options.wiki.getTiddlerLinks(title);
-        }
+		function byLinksInText(title) {
+			return directionOfTraverse === "to"
+				? options.wiki.getTiddlerBacklinks(title)
+				: options.wiki.getTiddlerLinks(title);
+		}
 
-        function byField(title) {
-            return directionOfTraverse === "to"
-                ? options.wiki.findListingsOfTiddler(title, fieldOfRelationship)
-                : options.wiki.getTiddlerList(title, fieldOfRelationship);
-        }
-    };
+		function byField(title) {
+			return directionOfTraverse === "to"
+				? options.wiki.findListingsOfTiddler(title,fieldOfRelationship)
+				: options.wiki.getTiddlerList(title,fieldOfRelationship);
+		}
+	};
 
-    /*
-    Get direction of traverse: field direction + optional invert direction
+	/*
+	Get direction of traverse: field direction + optional invert direction
 
-    Input: contextState
-    Param: field of relationship
-    */
-    exports["locator-direction-of-traverse"] = function (source, operator, options) {
-        var results = [];
+	Input: contextState
+	Param: field of relationship
+	*/
+	exports["locator-direction-of-traverse"] = function(source,operator,options) {
+		var results = [];
 
-        source(function (tiddler, title) {
-            results = [getDirectionOfTraverse(options, title, operator.operand)];
-        });
+		source(function(tiddler,title) {
+			results = [getDirectionOfTraverse(options,title,operator.operand)];
+		});
 
-        return results;
-    }
+		return results;
+	};
 
 })();
